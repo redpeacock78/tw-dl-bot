7b2948f9319337cb7bdd04fbadc552dd611040c8
redpeacock78 <scarletpeacock78@gmail.com>
2023-12-14 15:53:23 +0900

    feat: Change src/router/types/callbackTypes.ts

---
+++ src/router/types/callbackTypes.ts
@@ -0,0 +1,47 @@
+// deno-lint-ignore-file no-namespace
+import { Context, Env } from "hono";
+
+export namespace CallbackTypes {
+  export type bodyDataObject = {
+    status: "success" | "failure";
+    commandType: "dl";
+    actionType: "single" | "multi";
+    channel: string;
+    message: string;
+    token: string;
+    link: string;
+    name1: string;
+    name2?: string;
+    name3?: string;
+    name4?: string;
+    file1: File;
+    file2?: File;
+    file3?: File;
+    file4?: File;
+    type: string;
+    content?: string;
+  };
+  export type ContextType = Context<
+    Env,
+    "/callback",
+    Record<string | number | symbol, never>
+  >;
+  export namespace Actions {
+    export type callbackSuccess = {
+      [key: string]: {
+        [key: string]: {
+          [key: string]: (
+            c: ContextType,
+            body: CallbackTypes.bodyDataObject
+          ) => Promise<void>;
+        };
+      };
+    };
+    export type callbackFailure = {
+      [key: string]: (
+        c: ContextType,
+        body: CallbackTypes.bodyDataObject
+      ) => Promise<void>;
+    };
+  }
+}
