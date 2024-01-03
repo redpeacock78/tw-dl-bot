5baf0c1fea3362aec8e209da8ac3caa693788f17
redpeacock78 <scarletpeacock78@gmail.com>
2024-01-03 10:04:00 +0900

    add: Add src/libs/constants.ts

---
+++ src/libs/constants.ts
@@ -0,0 +1,14 @@
+export const Constants = {
+  UPDATE_TIME_LIMIT: 900000,
+  CallbackObject: {
+    Status: {
+      SUCCESS: "success",
+      FAILURE: "failure",
+      PROGRESS: "progress",
+    },
+    Oversize: {
+      TRUE: "true",
+      FALSE: "false",
+    },
+  },
+};
