ebbfcdeb913aa9069859f8b5340552a27feca43a
redpeacock78 <scarletpeacock78@gmail.com>
2024-01-14 17:35:40 +0900

    chore: Change src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { Hono } from "hono";
 import { serve } from "serve";
 import { startBot } from "discordeno";
+import api from "@router";
 import bot from "@bot/bot.ts";
-import api from "@router/index.ts";
 
 const app = new Hono();
 app.route("/", api);
 
-startBot(bot);
-serve(app.fetch);
+startBot(bot)
+  .then((): Promise<void> => serve(app.fetch))
+  .catch((): void => console.error("Failed!"));
