f0e23f8c6cfa4066e1605fbd9b767b42ab0291be
redpeacock78 <scarletpeacock78@gmail.com>
2024-01-16 10:29:39 +0900

    chore: Change src/router/callback.ts

--- src/router/callback.ts
+++ src/router/callback.ts
@@ -18,26 +18,19 @@ callback.post(
     } catch (_e) {
       body = (await c.req.parseBody()) as CallbackTypes.bodyDataObject;
     }
-    if (body.status === Constants.CallbackObject.Status.SUCCESS)
-      return await callbackSuccessFunctions[body.status][body.commandType!]
-        [body.actionType!](c, body)
-        .finally((): void => {
-          body = null;
-        });
-    if (body.status === Constants.CallbackObject.Status.FAILURE)
-      return await callbackFailureFunctions[body.status](c, body).finally(
-        (): void => {
-          body = null;
-        }
-      );
-    if (body.status === Constants.CallbackObject.Status.PROGRESS)
-      return await callbackProgressFunctions[body.status](c, body).finally(
-        (): void => {
-          body = null;
-        }
-      );
-    body = null;
-    return c.body(null, 200);
+    try {
+      if (body.status === Constants.CallbackObject.Status.SUCCESS)
+        return await callbackSuccessFunctions[body.status][body.commandType!][
+          body.actionType!
+        ](c, body);
+      if (body.status === Constants.CallbackObject.Status.FAILURE)
+        return await callbackFailureFunctions[body.status](c, body);
+      if (body.status === Constants.CallbackObject.Status.PROGRESS)
+        return await callbackProgressFunctions[body.status](c, body);
+      return c.body(null, 200);
+    } finally {
+      body = null;
+    }
   }
 );
 
