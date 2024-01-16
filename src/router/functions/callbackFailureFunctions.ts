e01c93579ad86fc6147634d6b877437e78c19068
redpeacock78 <scarletpeacock78@gmail.com>
2024-01-16 11:44:16 +0900

    chore: Change src/router/functions/callbackFailureFunctions.ts

--- src/router/functions/callbackFailureFunctions.ts
+++ src/router/functions/callbackFailureFunctions.ts
@@ -1,36 +1,49 @@
 import bot from "@bot/bot.ts";
-import { millisecondChangeFormat } from "@utils";
+import { Constants } from "@libs";
 import { CallbackTypes } from "@router/types/callbackTypes.ts";
+import { createFailureMessage } from "@router/messages/index.ts";
 
 const callbackFailureFunctions: CallbackTypes.Functions.callbackFailure = {
   failure: async (
     c: CallbackTypes.contextType,
     body: CallbackTypes.bodyDataObject | null
-  ): Promise<Response> =>
-    await bot.helpers
-      .editFollowupMessage(`${body!.token}`, `${body!.message}`, {
-        content: "**❌Failure!**",
-        embeds: [
-          {
-            fields: [
-              { name: "#️⃣ Run Number", value: `> \`#${body!.number}\`` },
-              {
-                name: "🕑 Total Time",
-                value: `> \`${millisecondChangeFormat(
-                  new Date().getTime() - Number(body!.startTime)
-                )}\``,
-              },
-              { name: "🔗 Tweet URL", value: `> ${body!.link}` },
-            ],
-            description: body!.content,
-            color: 0x4db56a,
-            timestamp: new Date().getTime(),
-          },
-        ],
-      })
+  ): Promise<Response> => {
+    const runTime: number = new Date().getTime() - Number(body!.startTime);
+    const editFollowupMessageFlag: boolean =
+      runTime <= Constants.EDIT_FOLLOWUP_MESSAGE_TIME_LIMIT;
+    if (editFollowupMessageFlag)
+      return await bot.helpers
+        .editFollowupMessage(
+          `${body!.token}`,
+          `${body!.message}`,
+          createFailureMessage({
+            runNumber: body!.number,
+            runTime: runTime,
+            link: body!.link,
+            content: body!.content as string,
+            editFollowupMessageFlag: editFollowupMessageFlag,
+          })
+        )
+        .then((): Response => c.body(null, 204))
+        .catch((): Response => c.body(null, 500))
+        .finally((): null => (body = null));
+    return await bot.helpers
+      .sendMessage(
+        `${body!.channel}`,
+        createFailureMessage({
+          messageId: body!.message,
+          channelId: body!.channel,
+          runNumber: body!.number,
+          runTime: runTime,
+          link: body!.link,
+          content: body!.content as string,
+          editFollowupMessageFlag: editFollowupMessageFlag,
+        })
+      )
       .then((): Response => c.body(null, 204))
       .catch((): Response => c.body(null, 500))
-      .finally((): null => (body = null)),
+      .finally((): null => (body = null));
+  },
 };
 
 export default callbackFailureFunctions;
