0bc3e0dd15aa5890b037a1476de92fe520bfe803
redpeacock78 <scarletpeacock78@gmail.com>
2024-01-16 11:41:10 +0900

    add: Add src/router/types/createMessageTypes.ts

---
+++ src/router/types/createMessageTypes.ts
@@ -0,0 +1,27 @@
+// deno-lint-ignore-file no-namespace
+import { FileContent } from "discordeno";
+
+export namespace CreateMessageTypes {
+  export type successMessageInfoType = {
+    messageId?: string;
+    channelId?: string;
+    runNumber: string;
+    runTime: number;
+    totalSize: string;
+    fileName?: string;
+    fileNamesArray?: string[];
+    link: string;
+    file?: Blob;
+    filesArray?: FileContent[];
+    editFollowupMessageFlag: boolean;
+  };
+  export type failureMessageInfoType = {
+    messageId?: string;
+    channelId?: string;
+    runNumber: string;
+    runTime: number;
+    link: string;
+    content: string;
+    editFollowupMessageFlag: boolean;
+  };
+}
