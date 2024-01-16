31857367814f831711d631407ae2617cb3c05328
redpeacock78 <scarletpeacock78@gmail.com>
2024-01-16 11:43:39 +0900

    add: Add src/router/messages/createFailureMessage.ts

---
+++ src/router/messages/createFailureMessage.ts
@@ -0,0 +1,43 @@
+import { CreateMessage, InteractionCallbackData } from "discordeno";
+import { millisecondChangeFormat } from "@utils";
+import { CreateMessageTypes } from "@router/types/createMessageTypes.ts";
+
+const createFailureMessage = (
+  info: CreateMessageTypes.failureMessageInfo
+): CreateMessage | InteractionCallbackData => {
+  let message: CreateMessage | InteractionCallbackData | null = {
+    content: "**❌Failure!**",
+    embeds: [
+      {
+        fields: [
+          { name: "#️⃣ Run Number", value: `> \`#${info.runNumber}\`` },
+          {
+            name: "🕑 Total Time",
+            value: `> \`${millisecondChangeFormat(info.runTime)}\``,
+          },
+          { name: "🔗 Tweet URL", value: `> ${info.link}` },
+        ],
+        description: info.content,
+        color: 0x4db56a,
+        timestamp: new Date().getTime(),
+      },
+    ],
+  };
+  if (!info.editFollowupMessageFlag) {
+    const messageReference = {
+      messageReference: {
+        messageId: `${info.messageId}`,
+        channelId: `${info.channelId}`,
+        failIfNotExists: true,
+      },
+    };
+    Object.assign(message, messageReference);
+  }
+  try {
+    return message;
+  } finally {
+    message = null;
+  }
+};
+
+export default createFailureMessage;
