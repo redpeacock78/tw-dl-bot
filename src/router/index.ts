4aff0361d5aed4b8d136287a8221147102d40f18
redpeacock78 <scarletpeacock78@gmail.com>
2024-01-16 13:17:16 +0900

    chore: Change src/router/index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from "hono";
+import { Constants } from "@libs";
 import { default as ping } from "@router/ping.ts";
 import { default as callback } from "@router/callback.ts";
+import { CallbackTypes } from "@router/types/callbackTypes.ts";
 
-const api = new Hono().basePath("/api");
+const api: CallbackTypes.honoType<typeof Constants.BASE_PATH> =
+  new Hono().basePath(Constants.BASE_PATH);
 
 api.route("/", ping);
 api.route("/", callback);
