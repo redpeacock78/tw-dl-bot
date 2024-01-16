c990072f4b03da5acd2510895c15395a4f0ba872
redpeacock78 <scarletpeacock78@gmail.com>
2024-01-16 16:29:35 +0900

    chore: Change src/libs/constants.ts

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -15,4 +15,10 @@ export const Constants = {
       FALSE: "false",
     },
   },
+  HttpStatus: {
+    OK: 200,
+    NO_CONTENT: 204,
+    BAD_REQUEST: 400,
+    INTERNAL_SERVER_ERROR: 500,
+  },
 };
