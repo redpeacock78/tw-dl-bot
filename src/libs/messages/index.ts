8220a2f232efcf6187b87cdfbcde4d9694f009a6
redpeacock78 <scarletpeacock78@gmail.com>
2024-01-20 12:47:28 +0900

    chore: Change src/libs/messages/index.ts

--- src/libs/messages/index.ts
+++ src/libs/messages/index.ts
@@ -2,4 +2,8 @@ import createSuccessMessage from "@router/messages/createSuccessMessage.ts";
 import createFailureMessage from "@router/messages/createFailureMessage.ts";
 import createProgressMessage from "@router/messages/createProgressMessage.ts";
 
-export { createSuccessMessage, createFailureMessage, createProgressMessage };
+export const messages = {
+  createSuccessMessage,
+  createFailureMessage,
+  createProgressMessage,
+};
