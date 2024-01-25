88eb1fb332794e657556ef70ee284e8372abbdc0
redpeacock78 <scarletpeacock78@gmail.com>
2024-01-25 16:32:11 +0900

    add: Add src/libs/webhook.ts

---
+++ src/libs/webhook.ts
@@ -0,0 +1,27 @@
+import { Secrets, Constants } from "@libs";
+import ky, { KyResponse } from "ky";
+
+export const webhook = async (message: {
+  content: string;
+  channelId: bigint;
+  id: bigint;
+  token: string;
+}): Promise<KyResponse> => {
+  return await ky.post(Secrets.DISPATCH_URL, {
+    json: {
+      event_type: Constants.Webhook.Json.EVENT_TYPE,
+      client_payload: {
+        commandType: Constants.Webhook.Json.ClientPayload.COMMAND_TYPE,
+        link: message.content,
+        channel: `${message.channelId}`,
+        message: `${message.id}`,
+        token: message.token,
+        startTime: new Date().getTime().toString(),
+      },
+    },
+    headers: {
+      Authorization: `token ${Secrets.GITHUB_TOKEN}`,
+      Accept: Constants.Webhook.Headers.ACCEPT,
+    },
+  });
+};
