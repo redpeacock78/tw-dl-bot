e57ceadfb64593f2e80724c0790120bc29b1554a
redpeacock78 <scarletpeacock78@gmail.com>
2024-01-25 17:57:36 +0900

    chore: Change src/libs/index.ts

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,6 +1,7 @@
 export { isUrl } from "@libs/isUrl.ts";
 export { isEmptyObj } from "@libs/isEmptyObj.ts";
 export { webhook } from "@libs/webhook.ts";
+export { multiFilesContent } from "@libs/multiFilesContent.ts";
 export { Secrets } from "@libs/secrets.ts";
 export { Constants } from "@libs/constants.ts";
 export { Messages } from "@libs/messages/index.ts";
