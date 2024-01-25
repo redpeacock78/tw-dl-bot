45d17f35eb4fe923c89cbcd143ea9675295a1af5
redpeacock78 <scarletpeacock78@gmail.com>
2024-01-25 18:28:37 +0900

    add: Add src/libs/contents/index.ts

---
+++ src/libs/contents/index.ts
@@ -0,0 +1,7 @@
+import fileContent from "@libs/contents/filesContent.ts";
+import multiFilesContent from "@libs/contents/multiFilesContent.ts";
+
+export const Contents = {
+  fileContent,
+  multiFilesContent,
+};
