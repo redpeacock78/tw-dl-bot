991a7c74173e034b660d43d781c53b5c0bb633df
redpeacock78 <scarletpeacock78@gmail.com>
2024-01-25 15:51:52 +0900

    chore: Change src/libs/messages/index.ts

--- src/libs/messages/index.ts
+++ src/libs/messages/index.ts
@@ -1,9 +1,11 @@
 import createSuccessMessage from "@libs/messages/createSuccessMessage.ts";
 import createFailureMessage from "@libs/messages/createFailureMessage.ts";
 import createProgressMessage from "@libs/messages/createProgressMessage.ts";
+import createErrorMessage from "@libs/messages/createErrorMessage.ts";
 
 export const Messages = {
   createSuccessMessage,
   createFailureMessage,
   createProgressMessage,
+  createErrorMessage,
 };
