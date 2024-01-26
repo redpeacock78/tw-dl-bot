e7d9059ef4b579acf0e9a69cc989a9952e999830
redpeacock78 <scarletpeacock78@gmail.com>
2024-01-26 16:33:57 +0900

    chore: Change src/router/callback.ts

--- src/router/callback.ts
+++ src/router/callback.ts
@@ -1,10 +1,6 @@
 import { Hono } from "hono";
 import { Constants } from "@libs";
-import {
-  callbackSuccessFunctions,
-  callbackFailureFunctions,
-  callbackProgressFunctions,
-} from "@router/functions/index.ts";
+import { Functions } from "@router/functions/index.ts";
 import { CallbackTypes } from "@router/types/callbackTypes.ts";
 
 const callback: CallbackTypes.honoType<"/"> = new Hono();
@@ -13,7 +9,7 @@ callback.post(
   Constants.CALLBACK_PATH,
   async (
     c: CallbackTypes.contextType<typeof Constants.CALLBACK_PATH>
-  ): Promise<Response | undefined> => {
+  ): Promise<Response> => {
     let body: CallbackTypes.bodyDataObject | null = null;
     try {
       body = (await c.req.raw.clone().json()) as CallbackTypes.bodyDataObject;
@@ -22,13 +18,13 @@ callback.post(
     }
     try {
       if (body.status === Constants.CallbackObject.Status.SUCCESS)
-        return await callbackSuccessFunctions[body.status][body.commandType!][
-          body.actionType!
-        ](c, body);
+        return await Functions.callbackSuccessFunctions.success[
+          body.commandType!
+        ][body.actionType!]({ c, body });
       if (body.status === Constants.CallbackObject.Status.FAILURE)
-        return await callbackFailureFunctions[body.status](c, body);
+        return await Functions.callbackFailureFunctions.failure({ c, body });
       if (body.status === Constants.CallbackObject.Status.PROGRESS)
-        return await callbackProgressFunctions[body.status](c, body);
+        return await Functions.callbackProgressFunctions.progress({ c, body });
       return c.body(null, Constants.HttpStatus.BAD_REQUEST);
     } finally {
       body = null;
