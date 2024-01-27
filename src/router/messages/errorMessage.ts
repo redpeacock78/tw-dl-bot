6a202bf00f8ad1066b0aa915ccf050af8bbedd03
redpeacock78 <scarletpeacock78@gmail.com>
2024-01-27 18:34:57 +0900

    chore: Change src/router/messages/errorMessage.ts

--- src/router/messages/errorMessage.ts
+++ src/router/messages/errorMessage.ts
@@ -1,13 +1,18 @@
 import bot from "@bot/bot.ts";
-import { Messages } from "@libs";
 import { Message } from "discordeno";
+import { Messages, Constants } from "@libs";
 import { CreateMessageTypes } from "@router/types/createMessageTypes.ts";
 
 const errorMessage = async (
   errorMessageObject: CreateMessageTypes.sendErrorMessageObject | null
 ): Promise<Message> => {
   try {
-    if (errorMessageObject!.editFollowupMessageFlag)
+    const runTime: number =
+      new Date().getTime() - Number(errorMessageObject!.startTime);
+    const editFollowupMessageFlag: boolean =
+      runTime <= Constants.EDIT_FOLLOWUP_MESSAGE_TIME_LIMIT ||
+      errorMessageObject!.oversize !== Constants.CallbackObject.Oversize.TRUE;
+    if (editFollowupMessageFlag)
       return await bot.helpers.editFollowupMessage(
         errorMessageObject!.token,
         errorMessageObject!.message,
