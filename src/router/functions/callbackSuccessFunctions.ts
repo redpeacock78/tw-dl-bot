c940c00dd052bcb19cca2c958633c718ea01ed2d
redpeacock78 <scarletpeacock78@gmail.com>
2024-01-27 18:35:15 +0900

    chore: Change src/router/functions/callbackSuccessFunctions.ts

--- src/router/functions/callbackSuccessFunctions.ts
+++ src/router/functions/callbackSuccessFunctions.ts
@@ -4,7 +4,7 @@ import { ContentsTypes } from "@libs/types/contentsTypes.ts";
 import { CallbackTypes } from "@router/types/callbackTypes.ts";
 
 const noContent: number = Constants.HttpStatus.NO_CONTENT;
-const internalServerError: number = Constants.HttpStatus.INTERNAL_SERVER_ERROR;
+const serverError: number = Constants.HttpStatus.INTERNAL_SERVER_ERROR;
 
 const callbackSuccessFunctions: CallbackTypes.Functions.callbackSuccess = {
   success: {
@@ -12,14 +12,24 @@ const callbackSuccessFunctions: CallbackTypes.Functions.callbackSuccess = {
       single: async <T extends string>(
         infoObject: CallbackTypes.infoObjectType<T>
       ): Promise<Response> => {
-        const runTime: number =
-          new Date().getTime() - Number(infoObject.body!.startTime);
-        const editFollowupMessageFlag: boolean =
-          runTime <= Constants.EDIT_FOLLOWUP_MESSAGE_TIME_LIMIT ||
-          infoObject.body!.oversize !== Constants.CallbackObject.Oversize.TRUE;
         let filesObject: ContentsTypes.singleFileContentObject | null;
         try {
           filesObject = await Contents.singleFileContent(infoObject.body!);
+          return await SendMessages.successMessage
+            .singleFile({
+              token: infoObject.body!.token,
+              channelId: infoObject.body!.channel,
+              messageId: infoObject.body!.message,
+              runNumber: infoObject.body!.number,
+              startTime: infoObject.body!.startTime,
+              totalSize: infoObject.body!.size!,
+              fileName: filesObject!.fileName,
+              link: infoObject.body!.link,
+              file: filesObject!.blobData,
+              oversize: infoObject.body!.oversize!,
+            })
+            .then((): Response => infoObject.c.body(null, noContent))
+            .catch((): Response => infoObject.c.body(null, serverError));
         } catch (e: unknown) {
           return await SendMessages.errorMessage({
             token: infoObject.body!.token,
@@ -28,43 +38,37 @@ const callbackSuccessFunctions: CallbackTypes.Functions.callbackSuccess = {
             number: infoObject.body!.number,
             link: infoObject.body!.link,
             description: (e as Error).message,
-            editFollowupMessageFlag: editFollowupMessageFlag,
+            startTime: infoObject.body!.startTime,
+            oversize: infoObject.body!.oversize!,
           })
             .then((): Response => infoObject.c.body(null, noContent))
-            .catch((): Response => infoObject.c.body(null, internalServerError))
-            .finally((): null => (infoObject.body = null));
+            .catch((): Response => infoObject.c.body(null, serverError));
+        } finally {
+          infoObject.body = null;
+          filesObject = null;
         }
-        return await SendMessages.successMessage
-          .singleFile({
-            token: infoObject.body!.token,
-            channelId: infoObject.body!.channel,
-            messageId: infoObject.body!.message,
-            runNumber: infoObject.body!.number,
-            runTime: runTime,
-            totalSize: infoObject.body!.size!,
-            fileName: filesObject!.fileName,
-            link: infoObject.body!.link,
-            file: filesObject!.blobData,
-            editFollowupMessageFlag: editFollowupMessageFlag,
-          })
-          .then((): Response => infoObject.c.body(null, noContent))
-          .catch((): Response => infoObject.c.body(null, internalServerError))
-          .finally((): void => {
-            infoObject.body = null;
-            filesObject = null;
-          });
       },
       multi: async <T extends string>(
         infoObject: CallbackTypes.infoObjectType<T>
       ): Promise<Response> => {
-        const runTime: number =
-          new Date().getTime() - Number(infoObject.body!.startTime);
-        const editFollowupMessageFlag: boolean =
-          runTime <= Constants.EDIT_FOLLOWUP_MESSAGE_TIME_LIMIT ||
-          infoObject.body!.oversize !== Constants.CallbackObject.Oversize.TRUE;
         let multiFilesObject: ContentsTypes.multiFilesContentObject | null;
         try {
           multiFilesObject = await Contents.multiFilesContent(infoObject.body!);
+          return await SendMessages.successMessage
+            .multiFiles({
+              token: infoObject.body!.token,
+              channelId: infoObject.body!.channel,
+              messageId: infoObject.body!.message,
+              runNumber: infoObject.body!.number,
+              startTime: infoObject.body!.startTime,
+              totalSize: infoObject.body!.size!,
+              fileNamesArray: multiFilesObject!.fileNamesArray,
+              link: infoObject.body!.link,
+              filesArray: multiFilesObject!.filesArray,
+              oversize: infoObject.body!.oversize!,
+            })
+            .then((): Response => infoObject.c.body(null, noContent))
+            .catch((): Response => infoObject.c.body(null, serverError));
         } catch (e: unknown) {
           return await SendMessages.errorMessage({
             token: infoObject.body!.token,
@@ -73,31 +77,15 @@ const callbackSuccessFunctions: CallbackTypes.Functions.callbackSuccess = {
             number: infoObject.body!.number,
             link: infoObject.body!.link,
             description: (e as Error).message,
-            editFollowupMessageFlag: editFollowupMessageFlag,
+            startTime: infoObject.body!.startTime,
+            oversize: infoObject.body!.oversize!,
           })
             .then((): Response => infoObject.c.body(null, noContent))
-            .catch((): Response => infoObject.c.body(null, internalServerError))
-            .finally((): null => (infoObject.body = null));
+            .catch((): Response => infoObject.c.body(null, serverError));
+        } finally {
+          infoObject.body = null;
+          multiFilesObject = null;
         }
-        return await SendMessages.successMessage
-          .multiFiles({
-            token: infoObject.body!.token,
-            channelId: infoObject.body!.channel,
-            messageId: infoObject.body!.message,
-            runNumber: infoObject.body!.number,
-            runTime: runTime,
-            totalSize: infoObject.body!.size!,
-            fileNamesArray: multiFilesObject!.fileNamesArray,
-            link: infoObject.body!.link,
-            filesArray: multiFilesObject!.filesArray,
-            editFollowupMessageFlag: editFollowupMessageFlag,
-          })
-          .then((): Response => infoObject.c.body(null, noContent))
-          .catch((): Response => infoObject.c.body(null, internalServerError))
-          .finally((): void => {
-            infoObject.body = null;
-            multiFilesObject = null;
-          });
       },
     },
   },
