710ac65541e2f62ba67ae448e27bfe5651fb210e
redpeacock78 <scarletpeacock78@gmail.com>
2024-01-27 18:34:32 +0900

    chore: Change src/router/messages/successMessage.ts

--- src/router/messages/successMessage.ts
+++ src/router/messages/successMessage.ts
@@ -1,6 +1,6 @@
 import bot from "@bot/bot.ts";
-import { Messages } from "@libs";
 import { Message } from "discordeno";
+import { Messages, Constants } from "@libs";
 import { CreateMessageTypes } from "@router/types/createMessageTypes.ts";
 
 const successMessage = {
@@ -8,13 +8,19 @@ const successMessage = {
     singleSuccsessMessageObject: CreateMessageTypes.SendSuccessMessage.singleFileObject | null
   ): Promise<Message> => {
     try {
-      if (singleSuccsessMessageObject!.editFollowupMessageFlag)
+      const runTime: number =
+        new Date().getTime() - Number(singleSuccsessMessageObject!.startTime);
+      const editFollowupMessageFlag: boolean =
+        runTime <= Constants.EDIT_FOLLOWUP_MESSAGE_TIME_LIMIT ||
+        singleSuccsessMessageObject!.oversize !==
+          Constants.CallbackObject.Oversize.TRUE;
+      if (editFollowupMessageFlag)
         return await bot.helpers.editFollowupMessage(
           singleSuccsessMessageObject!.token,
           singleSuccsessMessageObject!.messageId,
           Messages.createSuccessMessage({
             runNumber: singleSuccsessMessageObject!.runNumber,
-            runTime: singleSuccsessMessageObject!.runTime,
+            runTime: runTime,
             totalSize: singleSuccsessMessageObject!.totalSize,
             fileName: singleSuccsessMessageObject!.fileName,
             link: singleSuccsessMessageObject!.link,
@@ -27,7 +33,7 @@ const successMessage = {
           messageId: singleSuccsessMessageObject!.messageId,
           channelId: singleSuccsessMessageObject!.channelId,
           runNumber: singleSuccsessMessageObject!.runNumber,
-          runTime: singleSuccsessMessageObject!.runTime,
+          runTime: runTime,
           totalSize: singleSuccsessMessageObject!.totalSize,
           fileName: singleSuccsessMessageObject!.fileName,
           link: singleSuccsessMessageObject!.link,
@@ -42,13 +48,19 @@ const successMessage = {
     multiSuccsessMessageObject: CreateMessageTypes.SendSuccessMessage.multiFilesObject | null
   ): Promise<Message> => {
     try {
-      if (multiSuccsessMessageObject!.editFollowupMessageFlag)
+      const runTime: number =
+        new Date().getTime() - Number(multiSuccsessMessageObject!.startTime);
+      const editFollowupMessageFlag: boolean =
+        runTime <= Constants.EDIT_FOLLOWUP_MESSAGE_TIME_LIMIT ||
+        multiSuccsessMessageObject!.oversize !==
+          Constants.CallbackObject.Oversize.TRUE;
+      if (editFollowupMessageFlag)
         return await bot.helpers.editFollowupMessage(
           multiSuccsessMessageObject!.token,
           multiSuccsessMessageObject!.messageId,
           Messages.createSuccessMessage({
             runNumber: multiSuccsessMessageObject!.runNumber,
-            runTime: multiSuccsessMessageObject!.runTime,
+            runTime: runTime,
             totalSize: multiSuccsessMessageObject!.totalSize,
             fileNamesArray: multiSuccsessMessageObject!.fileNamesArray,
             link: multiSuccsessMessageObject!.link,
@@ -61,7 +73,7 @@ const successMessage = {
           messageId: multiSuccsessMessageObject!.messageId,
           channelId: multiSuccsessMessageObject!.channelId,
           runNumber: multiSuccsessMessageObject!.runNumber,
-          runTime: multiSuccsessMessageObject!.runTime,
+          runTime: runTime,
           totalSize: multiSuccsessMessageObject!.totalSize,
           fileNamesArray: multiSuccsessMessageObject!.fileNamesArray,
           link: multiSuccsessMessageObject!.link,
