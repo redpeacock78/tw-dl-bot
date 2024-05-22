05b9c35701b35d4c94d3bcad92f0f904d911627b
redpeacock78 <scarletpeacock78@gmail.com>
2024-05-22 18:08:32 +0900

    feat: Update src/libs/constants.ts

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -1,43 +1,50 @@
 export const Constants = {
-  ROOT_PATH: "/",
-  BASE_PATH: "/api",
-  PING_PATH: "/ping",
-  CALLBACK_PATH: "/callback",
-  EDIT_FOLLOWUP_MESSAGE_TIME_LIMIT: 900000,
+  ROOT_PATH: "/" as const,
+  BASE_PATH: "/api" as const,
+  PING_PATH: "/ping" as const,
+  CALLBACK_PATH: "/callback" as const,
+  EDIT_FOLLOWUP_MESSAGE_TIME_LIMIT: 900000 as const,
   Message: {
     Color: {
-      SUCCESS: 0x4db56a,
-      FAILURE: 0xf1c40f,
-      PROGRESS: 0x3498db,
-      ERROR: 0xe74c3c,
+      SUCCESS: 0x4db56a as const,
+      FAILURE: 0xf1c40f as const,
+      PROGRESS: 0x3498db as const,
+      ERROR: 0xe74c3c as const,
     },
   },
   Webhook: {
     Json: {
-      EVENT_TYPE: "download",
+      EVENT_TYPE: "download" as const,
       ClientPayload: {
-        COMMAND_TYPE: "dl",
+        COMMAND_TYPE: "dl" as const,
       },
     },
     Headers: {
-      ACCEPT: "application/vnd.github.everest-preview+json",
+      ACCEPT: "application/vnd.github.everest-preview+json" as const,
     },
   },
   CallbackObject: {
     Status: {
-      SUCCESS: "success",
-      FAILURE: "failure",
-      PROGRESS: "progress",
+      SUCCESS: "success" as const,
+      FAILURE: "failure" as const,
+      PROGRESS: "progress" as const,
+    },
+    commandType: {
+      DL: "dl" as const,
+    },
+    actionType: {
+      SINGLE: "single" as const,
+      MULTI: "multi" as const,
     },
     Oversize: {
-      TRUE: "true",
-      FALSE: "false",
+      TRUE: "true" as const,
+      FALSE: "false" as const,
     },
   },
   HttpStatus: {
-    OK: 200,
-    NO_CONTENT: 204,
-    BAD_REQUEST: 400,
-    INTERNAL_SERVER_ERROR: 500,
+    OK: 200 as const,
+    NO_CONTENT: 204 as const,
+    BAD_REQUEST: 400 as const,
+    INTERNAL_SERVER_ERROR: 500 as const,
   },
 };
