e292e3832bfdafe87d05ab20a7c2c82cf5e7dcf1
redpeacock78 <scarletpeacock78@gmail.com>
2024-05-22 20:24:14 +0900

    fix: Fix src/router/types/callbackTypes.ts

--- src/router/types/callbackTypes.ts
+++ src/router/types/callbackTypes.ts
@@ -59,7 +59,7 @@ export namespace CallbackTypes {
     export type callbackProgress = {
       [key: string]: <T extends string>(
         infoObject: infoObjectType<T>
-      ) => Promise<Response>;
+      ) => Response | Promise<Response>;
     };
   }
 }
