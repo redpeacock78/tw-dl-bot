747ed2e85c21a80ae93a68bedcec5b4b88cfb6e3
redpeacock78 <scarletpeacock78@gmail.com>
2024-05-22 18:31:15 +0900

    fix: Fix src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { Hono } from "hono";
 import { serve } from "serve";
+import { Hono } from "hono";
 import { startBot } from "discordeno";
 import api from "@router";
 import bot from "@bot/bot.ts";
+import { Constants } from "@libs";
+import { CallbackTypes } from "@router/types/callbackTypes.ts";
 
 const app = new Hono();
-app.route("/", api);
+app.route("/", api as CallbackTypes.honoType<"" | typeof Constants.BASE_PATH>);
 
 startBot(bot)
   .then((): Promise<void> => serve(app.fetch))
