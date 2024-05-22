55f9ba00f3b6fc319f271b26eda21381e7d87a46
redpeacock78 <scarletpeacock78@gmail.com>
2024-05-22 18:08:52 +0900

    feat: Update src/router/callback.ts

--- src/router/callback.ts
+++ src/router/callback.ts
@@ -1,7 +1,38 @@
 import { Hono } from "hono";
+import { match, Pattern } from "ts-pattern";
 import { Constants } from "@libs";
 import { Functions } from "@router/functions/index.ts";
 import { CallbackTypes } from "@router/types/callbackTypes.ts";
+
+const pattern = {
+  success: {
+    dl: {
+      single: [
+        Constants.CallbackObject.Status.SUCCESS,
+        Constants.CallbackObject.commandType.DL,
+        Constants.CallbackObject.actionType.SINGLE,
+      ] as const,
+      multi: [
+        Constants.CallbackObject.Status.SUCCESS,
+        Constants.CallbackObject.commandType.DL,
+        Constants.CallbackObject.actionType.MULTI,
+      ] as const,
+    },
+  },
+  failure: [
+    Constants.CallbackObject.Status.FAILURE,
+    Pattern.nullish,
+    Pattern.nullish,
+  ] as const,
+  progress: [
+    Constants.CallbackObject.Status.PROGRESS,
+    Pattern.nullish,
+    Pattern.nullish,
+  ] as const,
+};
+const success = Functions.callbackSuccessFunctions.success;
+const failure = Functions.callbackFailureFunctions.failure;
+const progress = Functions.callbackProgressFunctions.progress;
 
 const callback: CallbackTypes.honoType<"/"> = new Hono();
 
@@ -17,15 +48,26 @@ callback.post(
       body = (await c.req.parseBody()) as CallbackTypes.bodyDataObject;
     }
     try {
-      if (body.status === Constants.CallbackObject.Status.SUCCESS)
-        return await Functions.callbackSuccessFunctions.success[
-          body.commandType!
-        ][body.actionType!]({ c, body });
-      if (body.status === Constants.CallbackObject.Status.FAILURE)
-        return await Functions.callbackFailureFunctions.failure({ c, body });
-      if (body.status === Constants.CallbackObject.Status.PROGRESS)
-        return await Functions.callbackProgressFunctions.progress({ c, body });
-      return c.body(null, Constants.HttpStatus.BAD_REQUEST);
+      return match([body.status, body!.commandType, body!.actionType])
+        .with(
+          pattern.success.dl.single,
+          async (): Promise<Response> => await success.dl.single({ c, body })
+        )
+        .with(
+          pattern.success.dl.multi,
+          async (): Promise<Response> => await success.dl.multi({ c, body })
+        )
+        .with(
+          pattern.failure,
+          async (): Promise<Response> => await failure({ c, body })
+        )
+        .with(
+          pattern.progress,
+          async (): Promise<Response> => await progress({ c, body })
+        )
+        .otherwise(
+          (): Response => c.body(null, Constants.HttpStatus.BAD_REQUEST)
+        );
     } finally {
       body = null;
     }
