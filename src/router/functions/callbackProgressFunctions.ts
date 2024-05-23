38eaa741729100ca8a010f487bc39ab831eb7642
redpeacock78 <scarletpeacock78@gmail.com>
2024-05-23 11:37:55 +0900

    fix: Fix src/router/functions/callbackProgressFunctions.ts

--- src/router/functions/callbackProgressFunctions.ts
+++ src/router/functions/callbackProgressFunctions.ts
@@ -15,38 +15,38 @@ const callbackProgressFunctions: CallbackTypes.Functions.callbackProgress = {
    */
   progress: <T extends string>(
     infoObject: CallbackTypes.infoObjectType<T>
-  ): Response | Promise<Response> => {
+  ): Promise<Response> => {
     const runTime: number =
       new Date().getTime() - Number(infoObject.body!.startTime);
     const isEditFollowupMessage: boolean =
       runTime <= Constants.EDIT_FOLLOWUP_MESSAGE_TIME_LIMIT;
-    try {
-      return match(isEditFollowupMessage)
-        .with(
-          true,
-          async (): Promise<Response> =>
-            await bot.helpers
-              .editFollowupMessage(
-                infoObject.body!.token,
-                infoObject.body!.message,
-                Messages.createProgressMessage({
-                  runNumber: infoObject.body!.number,
-                  runTime: runTime,
-                  link: infoObject.body!.link,
-                  content: infoObject.body!.content!,
-                })
-              )
-              .then((): Response => infoObject.c.body(null, noContent))
-              .catch(
-                (): Response => infoObject.c.body(null, internalServerError)
-              )
-              .finally((): null => (infoObject.body = null))
-        )
-        .with(false, (): Response => infoObject.c.body(null, noContent))
-        .exhaustive();
-    } finally {
-      infoObject.body = null;
-    }
+    return match(isEditFollowupMessage)
+      .with(
+        true,
+        async (): Promise<Response> =>
+          await bot.helpers
+            .editFollowupMessage(
+              infoObject.body!.token,
+              infoObject.body!.message,
+              Messages.createProgressMessage({
+                runNumber: infoObject.body!.number,
+                runTime: runTime,
+                link: infoObject.body!.link,
+                content: infoObject.body!.content!,
+              })
+            )
+            .then((): Response => infoObject.c.body(null, noContent))
+            .catch((): Response => infoObject.c.body(null, internalServerError))
+            .finally((): null => (infoObject.body = null))
+      )
+      .with(
+        false,
+        (): Promise<Response> =>
+          Promise.resolve(infoObject.c.body(null, noContent)).finally(
+            (): null => (infoObject.body = null)
+          )
+      )
+      .exhaustive();
   },
 };
 
