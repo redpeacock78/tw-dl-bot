8d2eed61e90b408ed554f2c2b6b985919bfc68b6
redpeacock78 <scarletpeacock78@gmail.com>
2024-08-03 03:58:28 +0900

    fix: Fix src/libs/secrets.ts

--- src/libs/secrets.ts
+++ src/libs/secrets.ts
@@ -1,19 +1,19 @@
 import unienv from "unienv";
 
-const discordToken = unienv.get("DISCORD_TOKEN");
 const dispatchUrl = unienv.get("DISPATCH_URL");
 const githubToken = unienv.get("GITHUB_TOKEN");
+const discordToken = unienv.get("DISCORD_TOKEN");
 
-if (discordToken.isNg()) throw new Error(discordToken.error.message);
 if (dispatchUrl.isNg()) throw new Error(dispatchUrl.error.message);
 if (githubToken.isNg()) throw new Error(githubToken.error.message);
+if (discordToken.isNg()) throw new Error(discordToken.error.message);
 
-if (!discordToken.value) throw new Error("DISCORD_TOKEN is not set.");
 if (!dispatchUrl.value) throw new Error("DISPATCH_URL is not set.");
 if (!githubToken.value) throw new Error("GITHUB_TOKEN is not set.");
+if (!discordToken.value) throw new Error("DISCORD_TOKEN is not set.");
 
 export const Secrets = {
-  DISCORD_TOKEN: discordToken.value,
   DISPATCH_URL: dispatchUrl.value,
   GITHUB_TOKEN: githubToken.value,
+  DISCORD_TOKEN: discordToken.value,
 };
