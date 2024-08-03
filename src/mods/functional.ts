1e3d386d7558ca5849410fb08b0a6f29def5fa77
redpeacock78 <scarletpeacock78@gmail.com>
2024-08-03 14:23:39 +0900

    add: Add src/mods/functional.ts

---
+++ src/mods/functional.ts
@@ -0,0 +1,7 @@
+import * as Option from "fp-ts/Option";
+import * as Either from "fp-ts/Either";
+import * as Function from "fp-ts/function";
+import * as TaskEither from "fp-ts/TaskEither";
+import Pattern, { Pattern as P } from "ts-pattern";
+
+export { Option, Either, Function, TaskEither, Pattern, P };
