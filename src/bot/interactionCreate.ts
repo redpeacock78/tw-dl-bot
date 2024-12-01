49c01bce2c89dfd0715c938656470f15f7700c9d
redpeacock78 <scarletpeacock78@gmail.com>
2024-12-02 00:05:55 +0900

    fix: Fix src/bot/interactionCreate.ts

--- src/bot/interactionCreate.ts
+++ src/bot/interactionCreate.ts
@@ -1,3 +1,4 @@
+import { If } from "functional";
 import { KyResponse } from "ky";
 import { Messages, webhook, isUrl } from "@libs";
 import {
@@ -7,52 +8,69 @@ import {
   Message,
 } from "discordeno";
 
-export const interactionCreate = async (
-  b: Bot,
-  interaction: Interaction,
-  commandType: string
-) => {
-  const contents: string[] = interaction
-    .data!.options?.map((i) => i.value)
+/**
+ * Handles the interactionCreate event for the bot.
+ *
+ * @param {Bot} b - The bot instance.
+ * @param {Exclude<Pick<Interaction, "data">["data"], undefined>} data - The interaction data object.
+ * @param {Interaction} interaction - The interaction object.
+ * @param {string} commandType - The type of command, whether dl or threaddl.
+ * @return {Promise<void>} A promise that resolves when the function completes.
+ */
+export const interactionCreate = async (props: {
+  b: Bot;
+  data: Exclude<Pick<Interaction, "data">["data"], undefined>;
+  interaction: Interaction;
+  commandType: string;
+}): Promise<void> => {
+  const contents: string[] = props.data.options
+    ?.map((i) => i.value as string)
     .join("")
     .split(" ") as string[];
-  await b.helpers.sendInteractionResponse(interaction.id, interaction.token, {
-    type: InteractionResponseTypes.DeferredChannelMessageWithSource,
-  });
-  if (!contents.every((i: string): boolean => isUrl(i))) {
-    await b.helpers.sendFollowupMessage(interaction.token, {
-      type: InteractionResponseTypes.ChannelMessageWithSource,
-      data: Messages.createErrorMessage({
-        description: contents.join("\n"),
-      }),
-    });
-  } else {
+  await props.b.helpers.sendInteractionResponse(
+    props.interaction.id,
+    props.interaction.token,
+    {
+      type: InteractionResponseTypes.DeferredChannelMessageWithSource,
+    }
+  );
+  If(
+    !contents.every((i: string): boolean => isUrl(i)),
+    async (): Promise<void> => {
+      await props.b.helpers.sendFollowupMessage(props.interaction.token, {
+        type: InteractionResponseTypes.ChannelMessageWithSource,
+        data: Messages.createErrorMessage({
+          description: contents.join("\n"),
+        }),
+      });
+    }
+  ).else(async (): Promise<void> => {
     await Promise.all(
       contents.map(
         async (content: string): Promise<Message | KyResponse> =>
-          await b.helpers
-            .sendFollowupMessage(interaction.token, {
+          await props.b.helpers
+            .sendFollowupMessage(props.interaction.token, {
               type: InteractionResponseTypes.ChannelMessageWithSource,
               data: Messages.createProgressMessage({
                 content: `**🕑Queuing...**`,
-                link: contents.join("\n"),
+                link: content,
               }),
             })
             .then(
               async (i: Message): Promise<Message | KyResponse> =>
                 await webhook({
-                  commandType: commandType,
+                  commandType: props.commandType,
                   content: content,
                   channelId: i.channelId,
                   id: i.id,
-                  token: interaction.token,
+                  token: props.interaction.token,
                 }).catch(
                   async (e: Error): Promise<Message> =>
-                    await b.helpers.editFollowupMessage(
-                      interaction.token,
+                    await props.b.helpers.editFollowupMessage(
+                      props.interaction.token,
                       i.id,
                       Messages.createErrorMessage({
-                        link: contents.join("\n"),
+                        link: content,
                         description: e.message,
                       })
                     )
@@ -60,5 +78,5 @@ export const interactionCreate = async (
             )
       )
     );
-  }
+  });
 };
