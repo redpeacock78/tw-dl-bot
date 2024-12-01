aec2b9db654f03e4fab77cd64133fcd56f30ad22
redpeacock78 <scarletpeacock78@gmail.com>
2024-12-02 00:05:30 +0900

    fix: Fix src/mods/functional.ts

--- src/mods/functional.ts
+++ src/mods/functional.ts
@@ -3,5 +3,6 @@ import * as Either from "fp-ts/Either";
 import * as Function from "fp-ts/function";
 import * as TaskEither from "fp-ts/TaskEither";
 import { match as Match, Pattern as P } from "ts-pattern";
+import { If } from "expressionify";
 
-export { Option, Either, Function, TaskEither, Match, P };
+export { If, Option, Either, Function, TaskEither, Match, P };
