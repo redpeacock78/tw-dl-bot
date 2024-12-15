1a887837c395469282215de2fef7b1dc9cbcb468
redpeacock78 <scarletpeacock78@gmail.com>
2024-12-15 20:53:35 +0900

    chore: Change src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { serve } from "serve";
 import { Hono } from "hono";
-import { startBot } from "discordeno";
+import { startBot, editBotStatus, ActivityTypes } from "discordeno";
 import api from "@router";
 import bot from "@bot/bot.ts";
 import { Constants } from "@libs";
@@ -10,5 +10,32 @@ const app = new Hono();
 app.route("/", api as CallbackTypes.honoType<"" | typeof Constants.BASE_PATH>);
 
 startBot(bot)
-  .then((): Promise<void> => serve(app.fetch))
+  .then((): void => {
+    try {
+      serve(app.fetch);
+      setInterval(() => {
+        const usageMemory = Deno.memoryUsage().rss;
+        const totalMemory = Deno.systemMemoryInfo().total;
+        const memoryPercent = Number.parseFloat(
+          `${(usageMemory / totalMemory) * 100}`
+        ).toFixed(2);
+        try {
+          editBotStatus(bot, {
+            activities: [
+              {
+                name: `RAM Usage: ${memoryPercent}%`,
+                type: ActivityTypes.Game,
+                createdAt: Date.now(),
+              },
+            ],
+            status: "online",
+          });
+        } catch (e) {
+          throw new Error(e);
+        }
+      }, 10000);
+    } catch (e) {
+      throw new Error(e);
+    }
+  })
   .catch((): void => console.error("Failed!"));
