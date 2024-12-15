8927432789ceb6ddde54708fcbd727a1d5139c89
redpeacock78 <scarletpeacock78@gmail.com>
2024-12-16 04:12:16 +0900

    add: Add src/libs/bot/updateRAMUsage2BotStatus.ts

---
+++ src/libs/bot/updateRAMUsage2BotStatus.ts
@@ -0,0 +1,38 @@
+import { Constants } from "@libs";
+import { Bot, editBotStatus, ActivityTypes } from "discordeno";
+
+/**
+ * Updates the bot's status to its current RAM usage.
+ * @param {Bot} bot The bot instance.
+ * @returns {void}
+ */
+const updateRAMUsage2BotStatus = (bot: Bot): void => {
+  setInterval((): void => {
+    const freeMemory =
+      Deno.systemMemoryInfo().free +
+      Deno.systemMemoryInfo().buffers +
+      Deno.systemMemoryInfo().cached +
+      Deno.systemMemoryInfo().available;
+    const totalMemory = Deno.systemMemoryInfo().total;
+    const usageMemory = totalMemory - freeMemory;
+    const memoryPercent = Number.parseFloat(
+      `${(usageMemory / totalMemory) * 100}`
+    ).toFixed(2);
+    try {
+      editBotStatus(bot, {
+        activities: [
+          {
+            name: `RAM Usage: ${memoryPercent}%`,
+            type: ActivityTypes.Game,
+            createdAt: Date.now(),
+          },
+        ],
+        status: "online",
+      });
+    } catch (e) {
+      throw new Error(e);
+    }
+  }, Constants.UPDATE_BOT_STATUS_INTERVAL);
+};
+
+export default updateRAMUsage2BotStatus;
