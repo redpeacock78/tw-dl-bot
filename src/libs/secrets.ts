7c0778271a36a10607c7fa244b8b891c743841a0
redpeacock78 <scarletpeacock78@gmail.com>
2025-08-15 13:00:39 +0900

    fix: Fix src/libs/secrets.ts

--- src/libs/secrets.ts
+++ src/libs/secrets.ts
@@ -10,18 +10,20 @@ const isFullSecrets = (
 ): values is Record<SecretsKeys, string> =>
   Constants.SECRETS.every((i) => values[i] !== undefined);
 
-const props = {
-  values: {} as Partial<Record<SecretsKeys, string>>,
-  errors: [] as Array<string>,
-};
-const envs = Constants.SECRETS.reduce((acc, i): typeof props => {
-  const env = unienv.get(i);
-  return env.isNg()
-    ? { values: acc.values, errors: [...acc.errors, env.error.message] }
-    : !env.value
-    ? { values: acc.values, errors: [...acc.errors, `${i} is not set.`] }
-    : { values: { ...acc.values, [i]: env.value }, errors: acc.errors };
-}, props);
+const envs = Constants.SECRETS.reduce(
+  (acc, i) => {
+    const env = unienv.get(i);
+    return env.isNg()
+      ? { values: acc.values, errors: [...acc.errors, env.error.message] }
+      : !env.value
+      ? { values: acc.values, errors: [...acc.errors, `${i} is not set.`] }
+      : { values: { ...acc.values, [i]: env.value }, errors: acc.errors };
+  },
+  {
+    values: {} as Partial<Record<SecretsKeys, string>>,
+    errors: [] as Array<string>,
+  }
+);
 
 if (envs.errors.length > 0) throw new Error(envs.errors.join("\n"));
 if (!isFullSecrets(envs.values)) throw new Error("Not all secrets are set.");
