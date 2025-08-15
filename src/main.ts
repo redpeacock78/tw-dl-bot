6d29125ec241007e2be6fa70bf7eee80bd35016c
redpeacock78 <scarletpeacock78@gmail.com>
2025-08-15 13:29:27 +0900

    fix: Fix src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,10 @@ import { Constants, Bot } from "@libs";
 import { CallbackTypes } from "@router/types/callbackTypes.ts";
 
 const app = new Hono();
-app.route("/", api as CallbackTypes.honoType<"" | typeof Constants.BASE_PATH>);
+app.route(
+  Constants.ROOT_PATH,
+  api as CallbackTypes.honoType<"" | typeof Constants.BASE_PATH>
+);
 
 startBot(bot)
   .then((): void => {
