7d7eab113795b97b66de917b4f283dc5ec245bf9
redpeacock78 <scarletpeacock78@gmail.com>
2026-01-30 12:08:31 +0900

    fix: Fix src/router/functions/callbackSuccessFunctions.ts

--- src/router/functions/callbackSuccessFunctions.ts
+++ src/router/functions/callbackSuccessFunctions.ts
@@ -22,7 +22,7 @@ const callbackSuccessFunctions: CallbackTypes.Functions.callbackSuccess = {
        * @return {Promise<Response>} A promise that resolves to a Response object.
        */
       single: async <T extends string>(
-        infoObject: InfoObject<T>
+        infoObject: InfoObject<T>,
       ): Promise<Response> => {
         let filesObject: FileObject;
         try {
@@ -41,10 +41,10 @@ const callbackSuccessFunctions: CallbackTypes.Functions.callbackSuccess = {
               oversize: infoObject.body!.oversize!,
             })
             .then(
-              (): Response => infoObject.c.body(null, { status: noContent })
+              (): Response => infoObject.c.body(null, { status: noContent }),
             )
             .catch(
-              (): Response => infoObject.c.body(null, { status: serverError })
+              (): Response => infoObject.c.body(null, { status: serverError }),
             );
         } catch (e: unknown) {
           return await SendMessages.errorMessage({
@@ -58,10 +58,10 @@ const callbackSuccessFunctions: CallbackTypes.Functions.callbackSuccess = {
             oversize: infoObject.body!.oversize!,
           })
             .then(
-              (): Response => infoObject.c.body(null, { status: noContent })
+              (): Response => infoObject.c.body(null, { status: noContent }),
             )
             .catch(
-              (): Response => infoObject.c.body(null, { status: serverError })
+              (): Response => infoObject.c.body(null, { status: serverError }),
             );
         } finally {
           infoObject.body = null;
@@ -75,7 +75,7 @@ const callbackSuccessFunctions: CallbackTypes.Functions.callbackSuccess = {
        * @return {Promise<Response>} A promise that resolves to a Response object.
        */
       multi: async <T extends string>(
-        infoObject: InfoObject<T>
+        infoObject: InfoObject<T>,
       ): Promise<Response> => {
         let multiFilesObject: FileObject;
         try {
@@ -94,10 +94,10 @@ const callbackSuccessFunctions: CallbackTypes.Functions.callbackSuccess = {
               oversize: infoObject.body!.oversize!,
             })
             .then(
-              (): Response => infoObject.c.body(null, { status: noContent })
+              (): Response => infoObject.c.body(null, { status: noContent }),
             )
             .catch(
-              (): Response => infoObject.c.body(null, { status: serverError })
+              (): Response => infoObject.c.body(null, { status: serverError }),
             );
         } catch (e: unknown) {
           return await SendMessages.errorMessage({
@@ -111,10 +111,120 @@ const callbackSuccessFunctions: CallbackTypes.Functions.callbackSuccess = {
             oversize: infoObject.body!.oversize!,
           })
             .then(
-              (): Response => infoObject.c.body(null, { status: noContent })
+              (): Response => infoObject.c.body(null, { status: noContent }),
             )
             .catch(
-              (): Response => infoObject.c.body(null, { status: serverError })
+              (): Response => infoObject.c.body(null, { status: serverError }),
+            );
+        } finally {
+          infoObject.body = null;
+          multiFilesObject = null;
+        }
+      },
+    },
+    dlSpoiler: {
+      /**
+       * Asynchronously handles a single callback success event with a spoiler.
+       *
+       * @param {InfoObject<T>} infoObject - The information object containing the necessary data for the callback success event.
+       * @return {Promise<Response>} A promise that resolves to a Response object.
+       */
+      single: async <T extends string>(
+        infoObject: InfoObject<T>,
+      ): Promise<Response> => {
+        let filesObject: FileObject;
+        try {
+          filesObject = await Contents.singleFileContent(infoObject.body!);
+          return await SendMessages.successMessage
+            .singleFile({
+              token: infoObject.body!.token,
+              channelId: infoObject.body!.channel,
+              messageId: infoObject.body!.message,
+              runNumber: infoObject.body!.number,
+              startTime: infoObject.body!.startTime,
+              totalSize: infoObject.body!.size!,
+              fileName: filesObject!.fileName,
+              link: infoObject.body!.link,
+              file: filesObject!.blobData,
+              oversize: infoObject.body!.oversize!,
+              spoiler: true,
+            })
+            .then(
+              (): Response => infoObject.c.body(null, { status: noContent }),
+            )
+            .catch(
+              (): Response => infoObject.c.body(null, { status: serverError }),
+            );
+        } catch (e: unknown) {
+          return await SendMessages.errorMessage({
+            token: infoObject.body!.token,
+            channel: infoObject.body!.channel,
+            message: infoObject.body!.message,
+            number: infoObject.body!.number,
+            link: infoObject.body!.link,
+            description: (e as Error).message,
+            startTime: infoObject.body!.startTime,
+            oversize: infoObject.body!.oversize!,
+          })
+            .then(
+              (): Response => infoObject.c.body(null, { status: noContent }),
+            )
+            .catch(
+              (): Response => infoObject.c.body(null, { status: serverError }),
+            );
+        } finally {
+          infoObject.body = null;
+          filesObject = null;
+        }
+      },
+      /**
+       * Asynchronously handles multiple callback success events with a spoiler.
+       *
+       * @param {InfoObject<T>} infoObject - The information object containing the necessary data for the callback success event.
+       * @return {Promise<Response>} A promise that resolves to a Response object.
+       */
+      multi: async <T extends string>(
+        infoObject: InfoObject<T>,
+      ): Promise<Response> => {
+        let multiFilesObject: FileObject;
+        try {
+          multiFilesObject = await Contents.multiFilesContent(infoObject.body!);
+          return await SendMessages.successMessage
+            .multiFiles({
+              token: infoObject.body!.token,
+              channelId: infoObject.body!.channel,
+              messageId: infoObject.body!.message,
+              runNumber: infoObject.body!.number,
+              startTime: infoObject.body!.startTime,
+              totalSize: infoObject.body!.size!,
+              fileNamesArray: multiFilesObject!.fileNamesArray,
+              link: infoObject.body!.link,
+              filesArray: multiFilesObject!.filesArray,
+              oversize: infoObject.body!.oversize!,
+              spoiler: true,
+            })
+            .then(
+              (): Response => infoObject.c.body(null, { status: noContent }),
+            )
+            .catch(
+              (): Response => infoObject.c.body(null, { status: serverError }),
+            );
+        } catch (e: unknown) {
+          return await SendMessages.errorMessage({
+            token: infoObject.body!.token,
+            channel: infoObject.body!.channel,
+            message: infoObject.body!.message,
+            number: infoObject.body!.number,
+            link: infoObject.body!.link,
+            description: (e as Error).message,
+            startTime: infoObject.body!.startTime,
+            oversize: infoObject.body!.oversize!,
+          })
+            .then(
+              (): Response => infoObject.c.body(null, { status: noContent }),
+            )
+            .catch(
+              (): Response => infoObject.c.body(null, { status: serverError }),
             );
         } finally {
           infoObject.body = null;
