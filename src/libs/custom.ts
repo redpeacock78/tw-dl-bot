722563011205538550ebf5a84af4e7ee5a14bf7b
redpeacock78 <scarletpeacock78@gmail.com>
2026-01-30 12:07:25 +0900

    fix: Fix src/libs/custom.ts

--- src/libs/custom.ts
+++ src/libs/custom.ts
@@ -16,6 +16,18 @@ export const Custom = {
           Constants.CallbackObject.actionType.MULTI,
         ],
       },
+      DlSpoiler: {
+        Single: [
+          Constants.CallbackObject.Status.SUCCESS,
+          Constants.CallbackObject.commandType.DL_SPOILER,
+          Constants.CallbackObject.actionType.SINGLE,
+        ],
+        Multi: [
+          Constants.CallbackObject.Status.SUCCESS,
+          Constants.CallbackObject.commandType.DL_SPOILER,
+          Constants.CallbackObject.actionType.MULTI,
+        ],
+      },
     },
     Failure: [Constants.CallbackObject.Status.FAILURE, P.nullish, P.nullish],
     Progress: [Constants.CallbackObject.Status.PROGRESS, P.nullish, P.nullish],
